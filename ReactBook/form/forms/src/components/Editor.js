591f0f6aa2c88b8d22f1452245a3647e52c14356
Treek <nbatre1@yahoo.com>
2019-11-04 19:43:33 -0600

    keep moving forward

---
+++ ReactBook/form/forms/src/components/Editor.js
@@ -0,0 +1,20 @@
+import React,{Component} from 'react';
+
+
+
+
+
+class Example extends Component{
+
+
+
+    render(){
+
+        return <div className="hf bg-info text-white p-2">
+            Form will go here
+        </div>
+    }
+}
+
+
+export default Example;
